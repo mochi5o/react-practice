2eb6a74862dff3080eb1a0dacb62e68d33a7a495
mochi5o <1610.baumkuchen.124@gmail.com>
2020-05-13 22:00:50 +0900

    add Booklist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Booklist from './components/Booklist';
 
 const App = () => {
   return (
     <div>
       <h1>welcome to react app!!</h1>
+      <Booklist />
     </div>
   );
 }
